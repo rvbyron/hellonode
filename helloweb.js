974a70f252c423953090256c81e89076e5328c66
Bob Byron <bob@strategicweb.biz>
2015-04-07 15:26:33 -0500

    Node JS Web Application

--- helloweb.js
+++ helloweb.js
@@ -9,6 +9,21 @@ app.get("/", function (request, response) {
     response.send("Hello World!")
 });
 
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user');
+});
+
 // Listen on port 3000
 var server = app.listen(3000, function () {
 
